3bcd90627cc7b3ef458a6ccf56afaa76a03363ac
Roman Schmal.doz <Roman.Schmal.doz@wibizu.de>
2023-11-20 10:15:22 +0100

    Test

---
+++ GIT_aufgabe/A1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_aufgabe
+{
+    class A1
+    {
+        public static void Start()
+        {
+            int[] int_Array = new int[100];
+
+            Random rnd = new Random();
+            int x = rnd.Next(0, 100);            
+            //Hier wird eine Zufallszahl zwischen 0 und 99 erzeugt und in die Variable x gespeichert.
+            //Schreiben Sie im Bereich zwischen CODE START und CODE ENDE ihren Code rein.
+            // Befühlen Sie das Array mit 100 Zufallszahlen;
+
+            //CODE START
+
+            //CODE ENDE
+
+            foreach(var i in int_Array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ GIT_aufgabe/A2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_aufgabe
+{
+    class A2
+    {
+        public static void Start()
+        {
+            //Der Nutzer soll 10 Zahlen eingeben. Diese werden in das Array gespeichert. 
+            //Die Zahlen, die größte und die kleinste davon werden anschließend ausgegeben.
+            // Schreiben innerhalb von CODE START - CODE ENDE das Programm fertig
+            double[] dbl_Array = new double[10];
+            double max_Zahl = double.MinValue; //MinValue ist die kleinstmögliche Double Zahl die es gibt
+            double min_Zahl = double.MaxValue; //MaxValue ist die größtmögliche Double Zahl die es gibt
+            //CODE START
+
+            //CODE ENDE
+
+            Console.WriteLine("Ihre Eingaben waren: ");
+            foreach(var i in dbl_Array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n Die größte Zahl ist: " + max_Zahl);
+            Console.WriteLine("Die kleinste Zahl ist: " + min_Zahl);
+
+        }
+    }
+}
